4586d2ad1a4d9eba6596be07c8ddf016519f193d
sagar-developer08 <08sagar.s@gmail.com>
2025-06-22 20:37:08 +0530

    change in cms

--- src/api/config.js
+++ src/api/config.js
@@ -5,8 +5,8 @@ const axios = setupCache(Axios);
 
 const Api = axios.create({
   baseURL:
-    "http://localhost:8000/api",
-    // "loc://localhost:8000/api",
+    //  "https://oqnfmp6966.execute-api.us-east-1.amazonaws.com/dev/api",
+    "https://oqnfmp6966.execute-api.us-east-1.amazonaws.com/dev/api",
   // "https://o5whtqeiw2.execute-api.us-east-1.amazonaws.com/stagging/api",
   // "https://pign230wc2.execute-api.us-east-1.amazonaws.com/prod/api",
   timeout: 60000,
